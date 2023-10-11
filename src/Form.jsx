8d68b389f26a724aab180d463a0b571e1b4a52b5
ramanboff <bayramofframan@gmail.com>
2023-10-11 18:01:57 +0400

    Add button created

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,7 +1,14 @@
 const Form = () => {
   return ( 
     <form className="add-form">
-      <h3>What do you need for your 😍 trip>?</h3>
+      <h3>What do you need for your 😍 trip?</h3>
+      <select >
+      {Array.from({length:20}, (_,i) => i+1).map((num) => <option value={num} key={num}>
+        {num}
+      </option>)}
+      </select>
+      <input type="text" placeholder="item..." />
+      <button>Add</button>
     </form>
    );
 }
