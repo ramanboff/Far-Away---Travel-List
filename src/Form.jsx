e7de57e0098ba3672b5282f87335be21bb5fcece
ramanboff <bayramofframan@gmail.com>
2023-10-11 18:12:46 +0400

    form submit handleSubmit function created

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,16 +1,25 @@
 const Form = () => {
-  return ( 
-    <form className="add-form">
+
+const handleSubmit = (e) => {
+e.preventDefault()
+console.log(e)
+}
+
+
+  return (
+    <form onSubmit={handleSubmit} className="add-form">
       <h3>What do you need for your 😍 trip?</h3>
-      <select >
-      {Array.from({length:20}, (_,i) => i+1).map((num) => <option value={num} key={num}>
-        {num}
-      </option>)}
+      <select>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
       </select>
       <input type="text" placeholder="item..." />
       <button>Add</button>
     </form>
-   );
-}
- 
+  );
+};
+
 export default Form;
