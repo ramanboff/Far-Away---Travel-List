5cc616995bc369feb690edfb64cfeef310303204
ramanboff <bayramofframan@gmail.com>
2023-10-11 20:29:32 +0400

    new object created in the handleSubmit function

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,22 +1,34 @@
+import { useState } from "react";
+
 const Form = () => {
+
+const [description, setDescription] = useState("")
+const [quantity, setQuantity] = useState(1)
+
 
 const handleSubmit = (e) => {
 e.preventDefault()
-console.log(e)
+
+if(!description) return;
+const newItem = {description,quantity,package:false, id:Date.now()}
+console.log(newItem)
+setDescription("")
+setQuantity(1)
+
 }
 
 
   return (
     <form onSubmit={handleSubmit} className="add-form">
       <h3>What do you need for your 😍 trip?</h3>
-      <select>
+      <select onChange={(e) => setQuantity(+e.target.value)} value={quantity}>
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
           </option>
         ))}
       </select>
-      <input type="text" placeholder="item..." />
+      <input type="text" placeholder="item..." onChange={(e) => setDescription(e.target.value)} value={description}   />
       <button>Add</button>
     </form>
   );
