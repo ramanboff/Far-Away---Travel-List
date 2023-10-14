4426cdeb7831bac58a9065f1bf20620854100975
ramanboff <bayramofframan@gmail.com>
2023-10-15 00:42:14 +0400

    sort by desc sortByinput and sortBy packed functio

--- src/PackingList.jsx
+++ src/PackingList.jsx
@@ -1,22 +1,33 @@
 import { useState } from "react";
 import Item from "./Item";
 
-const PackingList = ({ items, onDeleteItem ,onToggleItem}) => {
+const PackingList = ({ items, onDeleteItem, onToggleItem }) => {
+  const [sortBy, setSortBy] = useState("input");
 
-const [sortBy, setSortBy] = useState("input")
+  let sortedItems;
+  if (sortBy === "input") sortedItems = items;
 
+  if (sortBy === "description")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
 
-
+      if(sortBy === "packed") sortedItems = items.slice().sort((a,b) => Number(a.packed) - Number(b.packed))
 
   return (
     <div className="list">
       <ul className="list">
-        {items.map((item) => (
-          <Item item={item} key={item.id} onDeleteItem={onDeleteItem} onToggleItem={onToggleItem}/>
+        {sortedItems.map((item) => (
+          <Item
+            item={item}
+            key={item.id}
+            onDeleteItem={onDeleteItem}
+            onToggleItem={onToggleItem}
+          />
         ))}
       </ul>
       <div className="actions">
-        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)} >
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
           <option value="input">Sort by input order</option>
           <option value="description">Sort by description</option>
           <option value="packed">Sort by packed Status</option>
